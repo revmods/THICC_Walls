63b56aa2d735725c5b73d0255aed6dd69909c5d5
revmods <113906380+revmods@users.noreply.github.com>
2022-09-25 12:37:41 -0500

    ops, moved to root

---
+++ 1.3/Source/THICCWall_Blueprint_Build.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+using RimWorld;
+
+namespace THICCWall {
+	public class THICCWall_Blueprint_Build : Blueprint_Build {
+		public override void Print(SectionLayer layer) {
+			THICCWall_Utility.Print(def, this, layer);
+		}
+	}
+}
---
+++ 1.3/Source/THICCWall_Building.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace THICCWall {
+	public class THICCWall_Building : Building {
+		public override void Print(SectionLayer layer) {
+			THICCWall_Utility.Print(def, this, layer);
+		}
+	}
+}
